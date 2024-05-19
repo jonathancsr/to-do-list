7efc773170b548fcc98a5ac55f0c1f71b19ed0ee
jonathancsr <jonathancsrr@gmail.com>
2024-05-19 20:47:39 -0300

    feat: improve number counter

--- src/components/TaskCounter/index.tsx
+++ src/components/TaskCounter/index.tsx
@@ -5,17 +5,29 @@ type TaskCounterProps = {
   completedTasks: number;
 }
 
-const StyledNumber = ({ number }: { number: number }) => {
+const StyledNumber = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className={styles.number}>{number}</div>
+    <div className={styles.number}>{children}</div>
   )
 }
+
+
 
 export const TaskCounter = ({ numberOfTasks = 0, completedTasks = 0 }: TaskCounterProps) => {
   return (
     <div className={styles.container}>
-      <p className={styles.color1}>Created Tasks <StyledNumber number={numberOfTasks} /></p>
-      <p className={styles.color2}>Completed <StyledNumber number={completedTasks} /></p>
+      <p className={styles.color1}>
+        Created Tasks
+        <StyledNumber>
+          {numberOfTasks}
+        </StyledNumber>
+      </p>
+      <p className={styles.color2}>
+        Completed
+        <StyledNumber>
+          {completedTasks} of {numberOfTasks}
+        </StyledNumber>
+      </p>
     </div>
   )
 }
