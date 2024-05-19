a662b8f8f18eb2859f00260d16659c5dd49544f8
jonathancsr <jonathancsrr@gmail.com>
2024-05-19 20:09:59 -0300

    feat: add task component

--- src/components/Tasks/Task.tsx
+++ src/components/Tasks/Task.tsx
@@ -1,12 +1,25 @@
 import { Task as TaskType } from "../../types";
+import Trash from "../icons/Trash";
 import styles from './styles.module.css';
 type TaskProps = {
   task: TaskType;
 }
 export const Task = ({ task }: TaskProps) => {
   return (
-    <div className={styles.container}>
+    <div
+    className={styles.container}
+
+    >
+      <div className={styles.checkbox}>
+        <input
+          type="checkbox"
+          checked={task.completed}
+        />
+      </div>
       <p>{task.title}</p>
+      <button>
+        <Trash />
+      </button>
     </div>
   )
 }
